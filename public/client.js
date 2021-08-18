f2f769cc23f9e95aee9e4e1a62a52cd17281ac31
Nathan Andrews <nandrews.programming@gmail.com>
2021-08-18 14:27:49 -0400

    Added userName functionality

--- public/client.js
+++ public/client.js
@@ -1,5 +1,24 @@
 const socket = io()
 
+const modalWrapper = document.getElementById('modal-wrapper')
+const modalForm = document.getElementById('modal-form')
+const modalInput = document.getElementById('modal-input')
+
+let userName = ''
+
+socket.on('connect', () => {
+  modalWrapper.style.display = 'flex'
+  modalForm.addEventListener('submit', e => {
+    e.preventDefault()
+    if (modalInput.value) {
+      socket.emit('new-user', modalInput.value)
+      userName = modalInput.value
+      modalInput.value = ''
+      modalWrapper.style.display = 'none'
+    }
+  })
+})
+
 const form = document.getElementById('form')
 const input = document.getElementById('input')
 
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
 
+let userName = ''
+
 app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => {
@@ -13,8 +15,12 @@ app.get('/', (req, res) => {
 })
 
 io.on('connection', (socket) => {
-  console.log(`${socket.id} has connected`)
-  socket.emit('chat-message', 'hello user')
+  console.log(`a new user with id: ${socket.id} has connected`)
+
+  socket.on('new-user', (name) => {
+    console.log(`user ${socket.id} has selected the username: ${name}`)
+    userName = name
+  })
 
   socket.on('chat-message', (msg) => {
     console.log('message: ' + msg)
